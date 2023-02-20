2a25177ecb86852568c389cd7fc2b8139f58087b
Jkig <DerekLeroux47@gmail.com>
2023-02-20 13:35:17 -0700

    Initializing react app, no funcitonality yet

---
+++ src/App.jsx
@@ -0,0 +1,21 @@
+import { useState } from 'react'
+import './App.css'
+
+function App() {
+  // state
+  const [on, setOn] = useState(false)
+
+  function goToPlanets() {
+    setOn(prev => !prev)
+  }
+
+  // now load planets when this is on, and don't when not..
+  return (
+    <div className="App">
+      <h1>Welcome to Planetarum</h1>
+      <button onClick={goToPlanets}>Go to planets!</button>
+    </div>
+  )
+}
+
+export default App
